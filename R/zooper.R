664e8782b10c739ac3682804b4c86f32ac90c2a0
sbashevkin <sam.bashevkin@deltacouncil.ca.gov>
2019-12-10 11:58:50 -0800

    Added descriptions of surveys to zooper documentation page to fix https://github.com/InteragencyEcologicalProgram/zooper/issues/1

--- R/zooper.R
+++ R/zooper.R
@@ -1,4 +1,4 @@
-#' zooper: A package for integrating zooplankton datasets collected by the Interagency Ecological Program from the Sacramento-San Joaquin Delta
+#' zooper: A package for integrating zooplankton datasets from the Sacramento San Joaquin Delta
 #'
 #' This package contains functions, lookup tables, and 2 built-in pre-combined datasets (one with the zooplankton data and another with the environmental data).
 #'
@@ -22,6 +22,14 @@
 #'   \item \code{\link{zoopComb}}
 #'   \item \code{\link{zoopEnvComb}}
 #' }
+#'
+#' @section Source datasets:
+#' \describe{
+#'   \item{Environmental Monitoring Program (EMP)}{The EMP zooplankton survey is run by the California Department of Fish and Wildlife. Zooplankton were first collected in 1972. It samples monthly at 17 fixed stations, 2 floating entrapment zone stations, and 3 stations in Carquinez Strait and San Pablo Bay that are only sampled during high outflow and low salinity conditions. EMP samples using micro (43 \eqn{\mu}m), meso (160 \eqn{\mu}m), and macro (505 \eqn{\mu}m) zooplankton nets. \href{ftp://ftp.wildlife.ca.gov/IEP_Zooplankton}{Data are available here.}}
+#'   \item{20-mm Survey}{The 20-mm survey is run by the California Department of Fish and Wildlife. Zooplankton were first collected in 1995. Zooplankton are collected concurrently with fish samples at 41-55 fixed open-channel stations per year. Samples are collected twice per month between March and July. Only Mesozooplankton are collected with a 160 \eqn{\mu}m mesh net. \href{ftp://ftp.dfg.ca.gov/Delta\%20Smelt}{Data are available here.}}
+#'   \item{Fall Midwater Trawl (FMWT) and Summer Townet Survey (TNS)}{The FMWT and TNS are run by the California Department of Fish and Wildlife. FMWT samples are collected monthly between September and December from a subset of the 122 fixed open-channel stations. STN samples are collected monthly between June and August from 40 fixed open-channel stations. Macrozooplankton have been collected since 2007 with a 505 \eqn{\mu}m mesh net while mesozooplankton have been collected since 2005 with a 160 \eqn{\mu}m mesh net. \href{ftp://ftp.dfg.ca.gov/TownetFallMidwaterTrawl/Zoopl_TownetFMWT}{Data are available here.}}
+#'   \item{Fish Restoration Program (FRP)}{FRP is run by the California Department of Fish and Wildlife. Zooplankton were first collected in 2015. Samples are collected monthly between March and December in shallow-water habitats near marshes. FRP samples with meso (150 \eqn{\mu}m) and macro (500 \eqn{\mu}m) zooplankton nets. \href{doi.org/10.6073/pasta/86810e72766ad19fccb1b9dd3955bdf8}{Data are available here.}}
+#'   }
 #'
 #' @docType package
 #' @name zooper
