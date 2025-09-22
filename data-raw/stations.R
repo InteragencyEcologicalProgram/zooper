8c2445a4eb73842ab1f0989a745995cd0fa77946
Lucy Andrews <lucy.andrews@water.ca.gov>
2025-09-22 16:48:28 -0700

    return to dropping NA observations from stations

--- data-raw/stations.R
+++ data-raw/stations.R
@@ -13,6 +13,7 @@ stationsEMPEZ<-read_csv(file.path("data-raw", "EZ_stations.csv"),
                         col_types = cols_only(SampleDate="c", StationCode="c", Lat="d", Long="d"))%>%
   select(Date=SampleDate, Station=StationCode, Latitude=Lat, Longitude=Long)%>%
   mutate(Date=parse_date_time(Date, "%m/%d/%y", tz="America/Los_Angeles"),
-         Station=recode(Station, EZ2="NZEZ2", EZ6="NZEZ6", EZ6SJR="NZEZ6SJR", EZ2SJR="NZEZ2SJR"))
+         Station=recode(Station, EZ2="NZEZ2", EZ6="NZEZ6", EZ6SJR="NZEZ6SJR", EZ2SJR="NZEZ2SJR"))%>%
+  drop_na()
 
 usethis::use_data(stations, stationsEMPEZ, overwrite = TRUE)
--- tests/testthat/test-stations.R
+++ tests/testthat/test-stations.R
@@ -1,11 +1,11 @@
 Stations<-zooper::stations
 Stations_EMPEZ<-zooper::stationsEMPEZ
 
-test_that("There are no NAs in the stations Source or Statio name", {
+test_that("There are no NAs in the stations Source or Station name", {
   expect_equal(length(which(is.na(Stations$Source))), 0)
   expect_equal(length(which(is.na(Stations$Station))), 0)
 })
 
-test_that("There are no NAs anywhere in the stationsEMPEZ dataset", {
+test_that("There are 0 NA values in the stationsEMPEZ dataset", {
   expect_equal(length(which(is.na(stationsEMPEZ))), 0)
 })
