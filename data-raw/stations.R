dfff9026cfeb6fbb1edc53fddcd0ff177783b16c
Lucy Andrews <lucy.andrews@water.ca.gov>
2025-09-22 16:45:24 -0700

    update date parsing for two-character year

--- data-raw/stations.R
+++ data-raw/stations.R
@@ -12,8 +12,7 @@ stations <- read_csv(file.path("data-raw", "stations.csv"),
 stationsEMPEZ<-read_csv(file.path("data-raw", "EZ_stations.csv"),
                         col_types = cols_only(SampleDate="c", StationCode="c", Lat="d", Long="d"))%>%
   select(Date=SampleDate, Station=StationCode, Latitude=Lat, Longitude=Long)%>%
-  mutate(Date=parse_date_time(Date, "%m/%d/%Y", tz="America/Los_Angeles"),
-         Station=recode(Station, EZ2="NZEZ2", EZ6="NZEZ6", EZ6SJR="NZEZ6SJR", EZ2SJR="NZEZ2SJR"))%>%
-  drop_na()
+  mutate(Date=parse_date_time(Date, "%m/%d/%y", tz="America/Los_Angeles"),
+         Station=recode(Station, EZ2="NZEZ2", EZ6="NZEZ6", EZ6SJR="NZEZ6SJR", EZ2SJR="NZEZ2SJR"))
 
 usethis::use_data(stations, stationsEMPEZ, overwrite = TRUE)
